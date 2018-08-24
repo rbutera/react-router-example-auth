399f19e491eb5e3c01431d4c4881814ba5843be1
Rai Butera <rai@rbutera.com>
2018-08-24 12:19:50 +0100

    hello world

---
+++ authentication/app/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import AuthExample from "./components/AuthExample"
+
+require("./index.css")
+
+ReactDOM.render(<AuthExample />, document.getElementById("auth-example-root"))
