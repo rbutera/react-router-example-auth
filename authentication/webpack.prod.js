9759c8c4aafec1424ea85708110731643b3ac0ca
Rai Butera <rai@rbutera.com>
2018-08-28 17:23:44 +0100

    fixed typo

--- authentication/webpack.prod.js
+++ authentication/webpack.prod.js
@@ -3,9 +3,6 @@ const common = require("./webpack.common.js")
 const BabelMinifyWebpackPlugin = require("babel-minify-webpack-plugin")
 
 module.exports = merge(common, {
-  mode: "production"
-},
-  plugins: [
-    new MinifyPlugin()
-  ]
-)
+  mode: "production",
+  plugins: [new MinifyPlugin()]
+})
